23e22b82efd76331d2f743a41c2499527ddade3f
onweekend <weijianwang74@gmail.com>
2025-06-03 22:05:42 +0800

    feat:执行prisma generate，修复数据库路径

--- packages/mastra/src/mastra/prisma/index.ts
+++ packages/mastra/src/mastra/prisma/index.ts
@@ -3,7 +3,7 @@ import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../../../generated/prisma";
 
 const adapter = new PrismaBetterSQLite3({
-  url: "file:./dev.db",
+  url: "file:../../prisma/dev.db",
 });
 
 export const prisma = new PrismaClient({ adapter });
